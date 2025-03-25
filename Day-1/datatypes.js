fdb566c61d918dc9b2669d89e5cf8571aebb41be
cboateng17 <carlosboateng360@gmail.com>
2025-03-25 10:23:04 +0000

    Day 1 of 30Days of JavaScript Assignment

---
+++ Day-1/datatypes.js
@@ -0,0 +1,7 @@
+// Q5. 
+// Checking Data Types
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof emptyValue);
+console.log(typeof schooling) ;
---
+++ Day-1/variable.js
@@ -0,0 +1,48 @@
+// Q4.
+// String Data Type
+let firstName = 'Carlos' 
+let lastName = 'Boateng'
+let country = 'Ghana'
+let city = 'Kumasi'
+
+let age = 25 // Number Data Type
+
+let isMarried = false // Boolean Data Type
+
+let schooling // Undefined Data Type
+
+let emptyValue = null // Null Data Type
+
+// Q6.
+// Declare four variables without assigning values
+let slim
+let boat
+let pay
+let allow
+
+// Q7.
+// Declare four variables with assigned values
+let isSleeping = false;
+let money = '200gh';
+const code = 233;
+const PI = 3.14;
+
+// Q8.
+// Declare variables in multiple lines
+let height;
+let weight;
+let citizen;
+let isAlive;
+
+// Q9. 
+// Declare variables in a single line
+let height, weight, citizen, isAlive;
+
+let myAge;
+let yourAge;
+
+myAge = 24;
+yourAge = 30;
+
+console.log("I am " , myAge,  " years old.");
+console.log("You are " yourAge " years old");
